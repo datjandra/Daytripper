36002961d55ea81524ba063bf415fde6aa8b9edb
Donny Tjandra <donny.tjandra@gmail.com>
2016-02-04 22:53:15 -0800

    push updates

--- src/com/vocifery/daytripper/service/UberRequestListener.java
+++
@@ -1,8 +0,0 @@
-package com.vocifery.daytripper.service;
-
-public interface UberRequestListener {
-	public void sendingRequest();
-	public void stopRequest();
-	public void onNoResponse();
-	public void onRequestSent();
-}
